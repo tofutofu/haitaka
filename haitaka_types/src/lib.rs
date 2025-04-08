79edc7c9ee4327edc16e8161a279cfcec2dd5708
Hans <hansgeunsmeyer@gmail.com>
2025-04-07 23:40:36 -0400

    Big switcheroo

---
+++ haitaka_types/src/lib.rs
@@ -0,0 +1,25 @@
+#![doc = include_str!("../README.md")]
+pub mod attacks;
+pub mod bitboard;
+pub mod color;
+pub mod file;
+pub mod helpers;
+pub mod piece;
+pub mod rank;
+pub mod shogi_move;
+pub mod sliders;
+pub mod square;
+
+pub mod board;
+
+pub use attacks::*;
+pub use bitboard::*;
+pub use color::*;
+pub use file::*;
+pub use piece::*;
+pub use rank::*;
+pub use shogi_move::*;
+pub use sliders::*;
+pub use square::*;
+
+pub use board::*;
